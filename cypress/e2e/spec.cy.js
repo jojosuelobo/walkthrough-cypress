4c5e541e19e86ae9a80d89fc1e413609a1f20dee
jojosuelobo <josuelobo2000@gmail.com>
2024-09-20 17:52:48 -0300

    desafio 1

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,40 @@
-describe('template spec', () => {
-  it('passes', () => {
+/* 
+Escreva um teste em Cypress que registre no console todas as opções dentro 
+da lista suspensa de comandos e também seus hrefs válidos em https://example.cypress.io/. 
+Além disso, verifique a existência de "Viewport" e "Network Requests".
+*/
+
+describe('desafio 1', () => {
+
+  it('registra no console opções da lista', () => {
+
     cy.visit('https://example.cypress.io')
+
+    cy.get('.dropdown-toggle').click();
+
+    const optionsList = [];
+
+    cy.get('.dropdown-menu li a').each(($el) => {
+      const text = $el.text();
+      const href = $el.attr('href');
+
+      optionsList.push(`Opção: ${text}, Href: ${href}`);
+
+    }).then(() => {
+
+      for (let i = 0; i < optionsList.length; i++) {
+        cy.log(optionsList[i]);
+      }
+      
+    });
   })
+
+  it('verifica a existência de Viewport e Network Requests', () => {
+    cy.visit('https://example.cypress.io')
+
+    cy.get('.dropdown-toggle').click();
+
+    cy.get('.dropdown-menu li a').contains('Viewport').should('exist');
+    cy.get('.dropdown-menu li a').contains('Network Requests').should('exist');
+  });
 })
